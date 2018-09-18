8fb0a9ef84354a7b24c15f74fb550dbaa9da60c1
rmccullagh <raymondpaulmccullagh@gmail.com>
2018-09-18 12:48:19 +1000

    added schema non-fabrication requirement for email in the Customer class.

--- WebHotel/WebHotel/Models/Customer.cs
+++ WebHotel/WebHotel/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace WebHotel.Models
     public class Customer
     {
         [Key, Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string email { get; set; }
         public String sName { get; set; }
         public String fName { get; set; }
